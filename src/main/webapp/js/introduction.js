b21d1c14114f8fe93adb77a60bfbb85c10cde544
sasaki renon <you@example.com>
2025-04-20 13:28:10 +0900

    CSSとｊｓ

---
+++ src/main/webapp/js/introduction.js
@@ -0,0 +1,20 @@
+//$('#mainvisual').slick();
+
+
+$('#mainvisual').slick({
+	autoplaySpeed:3000,
+	speed:1000,
+	autoplay:true,
+	infinite:true,
+	arrows:true,
+	prevArrow:'<div class="slick-prev"></div>',
+	nextArrow:'<div class="slick-next"></div>',
+	dots: true,
+	pauseOnFocus : false,
+	pauseOnHover : false,
+	pauseOnDotsHover:false,
+	
+})
+
+
+
